157a6b155f968de3340a228d35cf93e91c77e375
Ben Jacobs <hpcjaco1@login-q-4.data.cluster>
2023-05-26 08:10:49 +0100

    Updates

--- run_all_eqtl_analyses.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sbatch eqtl_icelake_ms_csf.sh
-sbatch eqtl_icelake_ms_pbmc.sh
-sbatch eqtl_icelake_oind_pbmc.sh
-sbatch eqtl_icelake_oind_csf.sh
-sbatch eqtl_icelake_nind_csf.sh
-sbatch eqtl_icelake_nind_pbmc.sh
-
